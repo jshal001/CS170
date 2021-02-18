ca5244a85f29161f18f1386c03af681d64f0180a
Jihad <jihadshalabi18@gmail.com>
2021-02-17 18:51:19 -0800

    Defined the node struct

--- search.cpp
+++ search.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
 
 using namespace std; 
 
 //the struct that will be used to represent each state/node
-struct state{ 
+struct Node{ 
     //this 2d array will be used to store the state
     vector<vector<int>> puzzle; 
     //cost g(i.e. the cost to arrive at the node)
@@ -13,8 +14,63 @@ struct state{
     int h = 0; 
     
     //f(n) = h(n) + g(n)
-    int f = 0; 
-}
+    int f = 0;   
+
+    Node(vector<vector<int>> userPuzzle, int h, int g){
+        this->puzzle = userPuzzle; 
+        this->h = h; 
+        this->g = g; 
+
+        this->f = g + h; 
+    }
+
+    void PrintNode(){
+
+        cout << "The best state to expand with a g(n) = " << this->g << " and h(n) = " << this->h << " is..." << endl; 
+
+        for(auto j: this->puzzle){
+            for(auto i: j){
+                cout << i << " ";
+            }
+            cout << endl; 
+        }
+
+        cout << "Expanding this node..." << endl; 
+
+    }
+}; 
+
+//TODO: Function for the general purpose search algorithm
+
+
+
+//TODO: Function to expand the tree 
+
+
+
+//TODO: The queuing function for A* misplaced tiles heuristic 
+
+
+
+//TODO: The queuing function for A* Manhattan 
+
+
+
+//TODO: The queuing function for uniform cost 
+
+
+
+
+
+//TODO: a function to check if the current node is a goal state
+
+
+
+
+
+//TODO: function to calculate f
+
+
 
 
 
@@ -34,7 +90,6 @@ int main(){
     }
     else{
         //have the user enter their puzzle
-        cout << 
     }
 
     
